b088388ce00ab92e20ed337ca79af32c73c1fdf6
syamjir <rsyamjir@gmail.com>
2024-12-05 21:08:56 +0530

    feat: add Firestore integration to send chat messages

---
+++ src/services/apiSendMessage.js
@@ -0,0 +1,16 @@
+import { collection, addDoc, serverTimestamp } from "firebase/firestore";
+import { db } from "../firebase/firebase"; // Firebase Firestore config
+
+// Send message to Firestore
+export const handleSendMessage = async (chatId, senderId, text) => {
+  try {
+    await addDoc(collection(db, "chats", chatId, "messages"), {
+      senderId,
+      text,
+      timestamp: serverTimestamp(),
+    });
+    console.log("send message successfully");
+  } catch (error) {
+    console.error("Error sending message:", error);
+  }
+};
