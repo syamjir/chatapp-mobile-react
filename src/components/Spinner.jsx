063d167752806e5a6ab36c13a1fda2f54e4b3740
syamjir <rsyamjir@gmail.com>
2024-12-12 20:06:40 +0530

    Add Spinner component for displaying a customizable loading animation

---
+++ src/components/Spinner.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Spinner({ onSize, containerPosition = "" }) {
+  return (
+    <div className={`absolute ${containerPosition}`}>
+      <div
+        className={` rounded-full  border-t-4 border-b-4 border-main animate-spin`}
+        style={{ width: `${onSize}px`, height: `${onSize}px` }}
+      ></div>
+    </div>
+  );
+}
+
+export default Spinner;
